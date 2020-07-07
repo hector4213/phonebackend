87f663a1fefd3f23df44d0e48e9a87a2f483a599
Hector C <hectorpwns@gmail.com>
2020-07-07 00:03:02 -0400

    uibuild

--- index.js
+++ index.js
@@ -1,119 +1,123 @@
-require("dotenv").config();
-const express = require("express");
-const morgan = require("morgan");
-const Person = require("./models/person");
-const app = express();
-const cors = require("cors");
 
-app.use(cors());
-app.use(express.static("build"));
-app.use(express.json());
+require('dotenv').config()
+const express = require('express')
+const morgan = require('morgan')
+const Person = require('./models/person')
+const app = express()
+const cors = require('cors')
+
+app.use(cors())
+app.use(express.static('build'))
+app.use(express.json())
 app.use(
   morgan(
-    ":method :url :status :res[content-length] - :response-time ms :dataSent"
+    ':method :url :status :res[content-length] - :response-time ms :dataSent'
   )
-);
+)
 
-morgan.token("dataSent", (req, res) => {
-  return JSON.stringify(req.body);
-});
+// eslint-disable-next-line no-unused-vars
+morgan.token('dataSent', (req, res) => {
+  return JSON.stringify(req.body)
+})
 
 //RESTFUL BEGINS HERE
 
-app.get("/", (req, res) => {
-  res.send("<h1>Hello People!</h1>");
-});
+app.get('/', (req, res) => {
+  res.send('<h1>Hello People!</h1>')
+})
 
-app.get("/info", (req, res) => {
-  const date = new Date();
+app.get('/info', (req, res) => {
+  const date = new Date()
 
   Person.countDocuments({}, (err, count) => {
     res.send(`Phonebook has info for ${count} people
 ${date}
-`);
-  });
-});
+`)
+  })
+})
 
-app.get("/api/persons", (req, res) => {
+app.get('/api/persons', (req, res) => {
   Person.find({}).then((people) => {
-    res.json(people);
-  });
-});
+    res.json(people)
+  })
+})
 
-app.get("/api/persons/:id", (req, res, next) => {
+app.get('/api/persons/:id', (req, res, next) => {
   Person.findById(req.params.id)
     .then((person) => {
       if (person) {
-        res.json(person);
+        res.json(person)
       } else {
-        res.status(404).end();
+        res.status(404).end()
       }
     })
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
-app.delete("/api/persons/:id", (req, res) => {
+app.delete('/api/persons/:id', (req, res, next) => {
   Person.findByIdAndRemove(req.params.id)
+    // eslint-disable-next-line no-unused-vars
     .then((result) => {
-      res.status(204).end();
+      res.status(204).end()
     })
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
-app.post("/api/persons", (req, res, next) => {
-  const person = req.body;
+app.post('/api/persons', (req, res, next) => {
+  const person = req.body
 
   const personToAdd = new Person({
     name: person.name,
     number: person.number,
-  });
+  })
 
   personToAdd
     .save()
     .then((savedPerson) => savedPerson.toJSON())
     .then((savedAndFormattedPerson) => res.json(savedAndFormattedPerson))
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
-app.put("/api/persons/:id", (req, res, next) => {
-  const person = req.body;
-  const id = req.params.id;
+app.put('/api/persons/:id', (req, res, next) => {
+  const person = req.body
+  const id = req.params.id
 
   const personToUpdate = {
     name: person.name,
     number: person.number,
-  };
+  }
 
   Person.findByIdAndUpdate(id, personToUpdate)
     .then((updatedPerson) => {
-      res.json(updatedPerson);
+      res.json(updatedPerson)
     })
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
 // Middleware is here
 
 const unknownEndpoint = (req, res) => {
-  res.status(404).send({ error: "unknown endpoint" });
-};
+  res.status(404).send({ error: 'unknown endpoint' })
+}
 
-app.use(unknownEndpoint) 
+app.use(unknownEndpoint)
 
 const errorHandler = (error, req, res, next) => {
   console.log(error.message)
-  if (error.name === "CastError") {
-    return res.status(400).send({ error: "malformatted id" });
-  } else if (error.name === "ValidationError") {
-    return res.status(400).json({ error: error.message });
+  if (error.name === 'CastError') {
+    return res.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return res.status(400).json({ error: error.message })
   }
 
-  next(error);
-};
+  next(error)
+}
 
-app.use(errorHandler);
+app.use(errorHandler)
 
-const PORT = process.env.PORT || 3001;
+// eslint-disable-next-line no-undef
+const PORT = process.env.PORT || 3001
 
 app.listen(PORT, () => {
-  console.log(`Server online running on PORT:${PORT}`);
-});
+  console.log(`Server online running on PORT:${PORT}`)
+})
--- mongo.js
+++ mongo.js
@@ -1,40 +1,42 @@
-const mongoose = require("mongoose");
+/* eslint-disable no-undef */
+const mongoose = require('mongoose')
 
 if (process.argv.length < 3) {
   console.log(
-    "Please provide the password as an argument: node mongo.js <password>"
-  );
-  process.exit(1);
+    'Please provide the password as an argument: node mongo.js <password>'
+  )
+  process.exit(1)
 }
 
-const password = process.argv[2];
+const password = process.argv[2]
 
-const url = `mongodb+srv://hectoradmin:${password}@cluster0.x2qgw.mongodb.net/phonebackend?retryWrites=true&w=majority`;
+const url = `mongodb+srv://hectoradmin:${password}@cluster0.x2qgw.mongodb.net/phonebackend?retryWrites=true&w=majority`
 
-mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
 
 const personSchema = new mongoose.Schema({
   name: String,
   number: String,
-});
+})
 
-const Person = mongoose.model("Person", personSchema);
+const Person = mongoose.model('Person', personSchema)
 
 const person = new Person({
   name: process.argv[3],
   number: process.argv[4],
-});
+})
 
+// eslint-disable-next-line no-unused-vars
 person.save().then((result) => {
   console.log(
     `Added ${process.argv[3]} number ${process.argv[4]} to phonebook`
-  );
-});
+  )
+})
 
 Person.find({}).then((results) => {
-  console.log("phonebook:");
+  console.log('phonebook:')
   results.forEach((person) => {
-    console.log(`${person.name} ${person.number}`);
-  });
-  mongoose.connection.close();
-});
+    console.log(`${person.name} ${person.number}`)
+  })
+  mongoose.connection.close()
+})
