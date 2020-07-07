25ee367fc635a545561d1dec64c9e6bf4c43bf3e
Hector C <hectorpwns@gmail.com>
2020-07-06 22:53:40 -0400

    uibuild

--- index.js
+++ index.js
@@ -1,42 +1,24 @@
+require("dotenv").config();
 const express = require("express");
-const morgan = require("morgan")
+const morgan = require("morgan");
+const Person = require("./models/person");
 const app = express();
-const cors = require('cors')
-app.use(cors())
+const cors = require("cors");
 
-
+app.use(cors());
+app.use(express.static("build"));
 app.use(express.json());
+app.use(
+  morgan(
+    ":method :url :status :res[content-length] - :response-time ms :dataSent"
+  )
+);
 
-morgan.token('dataSent', (req, res) =>{
-  return JSON.stringify(req.body)
-})
+morgan.token("dataSent", (req, res) => {
+  return JSON.stringify(req.body);
+});
 
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :dataSent'))
-
-app.use(express.static('build'))
-
-let persons = [
-  {
-    id: 1,
-    name: "Mia Clara",
-    number: "4165345833",
-  },
-  {
-    id: 2,
-    name: "Hector Clara",
-    number: "4165345833",
-  },
-  {
-    id: 3,
-    name: "Janella Clara",
-    number: "44162093971",
-  },
-  {
-    id: 4,
-    name: "Mateo Clara",
-    number: "4165555555",
-  },
-];
+//RESTFUL BEGINS HERE
 
 app.get("/", (req, res) => {
   res.send("<h1>Hello People!</h1>");
@@ -45,61 +27,92 @@ app.get("/", (req, res) => {
 app.get("/info", (req, res) => {
   const date = new Date();
 
-  res.send(`Phonebook has info for ${persons.length} people
+  Person.countDocuments({}, (err, count) => {
+    res.send(`Phonebook has info for ${count} people
 ${date}
 `);
+  });
 });
 
 app.get("/api/persons", (req, res) => {
-  res.json(persons);
+  Person.find({}).then((people) => {
+    res.json(people);
+  });
 });
 
-app.get("/api/persons/:id", (req, res) => {
-  const id = Number(req.params.id);
-  const person = persons.find((person) => person.id === id);
-
-  if (person) {
-    res.json(person);
-  } else {
-    res.status(404).end();
-  }
+app.get("/api/persons/:id", (req, res, next) => {
+  Person.findById(req.params.id)
+    .then((person) => {
+      if (person) {
+        res.json(person);
+      } else {
+        res.status(404).end();
+      }
+    })
+    .catch((error) => next(error));
 });
 
 app.delete("/api/persons/:id", (req, res) => {
-  const id = Number(req.params.id);
-  persons = persons.filter((person) => person.id !== id);
-  res.status(204).end();
+  Person.findByIdAndRemove(req.params.id)
+    .then((result) => {
+      res.status(204).end();
+    })
+    .catch((error) => next(error));
 });
 
-const generateRandomId = () => {
-  return Math.floor(Math.random() * 1000);
-};
-
-app.post("/api/persons", (req, res) => {
+app.post("/api/persons", (req, res, next) => {
   const person = req.body;
 
-  if (!person.name || !person.number) {
-    return res.status(404).json({
-      error: "missing information",
-    });
-  } else if (persons.some((per) => per.name === person.name)) {
-    return res.status(404).json({
-      error: "Name must be unique",
-    });
-  }
-
-  const personToAdd = {
+  const personToAdd = new Person({
     name: person.name,
     number: person.number,
-    id: generateRandomId(),
-  };
-  persons = persons.concat(personToAdd);
-  res.json(personToAdd);
+  });
+
+  personToAdd
+    .save()
+    .then((savedPerson) => savedPerson.toJSON())
+    .then((savedAndFormattedPerson) => res.json(savedAndFormattedPerson))
+    .catch((error) => next(error));
 });
 
+app.put("/api/persons/:id", (req, res, next) => {
+  const person = req.body;
+  const id = req.params.id;
 
+  const personToUpdate = {
+    name: person.name,
+    number: person.number,
+  };
 
-const PORT = process.env.PORT ||3001;
+  Person.findByIdAndUpdate(id, personToUpdate)
+    .then((updatedPerson) => {
+      res.json(updatedPerson);
+    })
+    .catch((error) => next(error));
+});
+
+// Middleware is here
+
+const unknownEndpoint = (req, res) => {
+  res.status(404).send({ error: "unknown endpoint" });
+};
+
+app.use(unknownEndpoint);
+
+const errorHandler = (error, req, res, next) => {
+  console.log(error.message);
+  if (error.name === "CastError") {
+    return res.status(400).send({ error: "malformatted id" });
+  } else if (error.name === "ValidationError") {
+    return res.status(400).send({ error: error.message });
+  }
+
+  next(error);
+};
+
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
   console.log(`Server online running on PORT:${PORT}`);
